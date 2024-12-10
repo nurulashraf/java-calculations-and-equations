0b60898860db33f30aa5393c755d80133c85f6e7
Nurul Ashraf <88407376+nurulashraf@users.noreply.github.com>
2024-12-10 20:54:31 +0800

    Add files via upload

---
+++ Q2_SolveEquations.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SolveEquations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+		// Get user input for the equations
+        System.out.println("Enter values for x, y, and z: \n");
+
+        System.out.print("x: ");
+        int x = scanner.nextInt();
+
+        System.out.print("y: ");
+        int y = scanner.nextInt();
+
+        System.out.print("z: ");
+        int z = scanner.nextInt();
+		
+		// Solve the equations
+        int result1 = x + y * z;
+        int result2 = (z * x) + y - 6;
+		
+		// Output the results
+        System.out.println("\nResult of x + y(z): " + result1);
+        System.out.println("Result of (z*x) + y - 6: " + result2);
+    }
+}
