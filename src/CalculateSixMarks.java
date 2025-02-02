8bc8603f4fd36320f13314d4bdf2f80ed8c55e27
Nurul Ashraf <88407376+nurulashraf@users.noreply.github.com>
2025-02-02 15:02:29 +0800

    Update CalculateSixMarks.java

--- src/CalculateSixMarks.java
+++ src/CalculateSixMarks.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class CalculateSixMarks {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         // Get user input for 6 marks
         System.out.println("Enter 6 marks: \n");
-        int mark1 = scanner.nextInt();
-        int mark2 = scanner.nextInt();
-        int mark3 = scanner.nextInt();
-        int mark4 = scanner.nextInt();
-        int mark5 = scanner.nextInt();
-        int mark6 = scanner.nextInt();
+        int mark1 = input.nextInt();
+        int mark2 = input.nextInt();
+        int mark3 = input.nextInt();
+        int mark4 = input.nextInt();
+        int mark5 = input.nextInt();
+        int mark6 = input.nextInt();
 
         // Calculate total marks
         int totalMarks = mark1 + mark2 + mark3 + mark4 + mark5 + mark6;
